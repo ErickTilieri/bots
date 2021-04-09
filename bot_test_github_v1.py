b97aaddfd44c1be31b05e65bcccad3abc8d3f129
ErickTilieri <80484845+ErickTilieri@users.noreply.github.com>
2021-04-09 19:05:01 -0300

    Update bot_test_github_v1.py
    added diferent css selector exception when trying to like the tweets

--- bot_test_github_v1.py
+++ bot_test_github_v1.py
@@ -27,14 +27,25 @@ class TwitterBot:
         bot = self.bot
         bot.get('https://twitter.com/search?q='+hashtag+'&src=typeahead_click')
         time.sleep(3)
+        bot.find_element_by_link_text("Latest").click()
         for i in range(6):
             bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')
             time.sleep(3)
             tweets = bot.find_elements_by_xpath('//div[@data-testid="tweet"]')
             for tweet in tweets:
-                bot.find_element_by_xpath('//div[@data-testid="like"]').click()
-                time.sleep(3)
-
-bot = TwitterBot('','') # Enter here your username/e-mail and password like this: ('email@email.com','password')
+                try:
+                    bot.find_element_by_xpath('//div[@data-testid="like"]').click()
+                    time.sleep(3)
+                except CssException1:
+                    bot.find_element_by_css_selector('.r-1ljd8xs > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > section:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > div:nth-child(1) > article:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(3) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)')
+                    time.sleep(3)
+                except CssException2:
+                    bot.find_element_by_css_selector('.r-1ljd8xs > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > section:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > article:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(3) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)')
+                    time.sleep(3)
+                except CssException3:
+                    bot.find_element_by_css_selector('.r-1ljd8xs > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > section:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > article:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(3) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)')
+                    time.sleep(3)
+bot = TwitterBot('User','password') # Enter here your username/e-mail and password like this: ('email@email.com','password')
 bot.login()
-bot.like_tweet('artificial intelligence') # Enter here the subject you want the bot to search for ('subject')
+bot.like_tweet('Subject here') # Enter here the subject you want the bot to search for ('subject')
+print('My job here is done')
